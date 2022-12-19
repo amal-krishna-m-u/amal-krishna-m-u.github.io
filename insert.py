f7220abab1d59a1874bc45a9e09471850c2d8e2b
amal <amalkrishna@ug.cusat.ac.in>
2022-12-19 19:54:10 +0530

    table creation

--- insert.py
+++ insert.py
@@ -2,14 +2,33 @@ import psycopg2
 
 # Connect to the database
 conn = psycopg2.connect(
-    host="localhost",
-    database="mydatabase",
-    user="user",
-    password="password"
+    host="mydatabase-instance.cis9c214rls8.ap-south-1.rds.amazonaws.com",
+    database="bda_prediction",
+    user="amal123",
+    password="Sinister003."
 )
 
 # Create a cursor to execute SQL statements
 cur = conn.cursor()
+
+
+
+
+# Define the table
+table = '''
+CREATE TABLE IF NOT EXISTS houses (
+    name TEXT,
+    rooms INTEGER,
+    location TEXT,
+    value REAL
+)
+'''
+
+# Execute the query
+cur.execute(table)
+
+# Commit the changes
+conn.commit()
 
 # Insert the form data into the database
 name = request.form['name']
